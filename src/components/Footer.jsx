661d9470bdc9e2b0aa85c8709c92da310618def4
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2024-10-08 03:13:28 -0300

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,77 +1,74 @@
+import byoerrLogo from '../assets/Byoerr-footer-logo.png';
+import facebookIcon from '../assets/Facebook.png';
+import twitterIcon from '../assets/Twitter.png';
+import instagramIcon from '../assets/Instagram.png';
+import linkedinIcon from '../assets/Linkedin.png';
+import youtubeIcon from '../assets/Youtube.png';
+
 const Footer = () => {
-    return (
-      <div className='flex pt-10 px-5 pb-10 flex-col gap-10 items-start self-stretch shrink-0 flex-nowrap bg-[#012e4d] relative z-[89]'>
-        <div className='flex flex-col md:flex-row justify-between items-start self-stretch shrink-0 flex-nowrap relative z-[90] w-full'>
-          <div className='flex w-full md:w-[272px] flex-col gap-6 items-start shrink-0 flex-nowrap relative z-[91]'>
-            <div className='flex w-full md:w-[116px] flex-col gap-4 justify-center items-start shrink-0 flex-nowrap relative z-[92]'>
-              <div className='w-[80px] h-[80px] shrink-0 bg-[url(../assets/images/638e2e55-aea7-4b59-a302-a4648fec618d.png)] bg-cover bg-no-repeat relative z-[93]' />
-              <span className="h-[43px] shrink-0 basis-auto font-['Inter'] text-[36px] font-semibold leading-[43px] text-[#fff] relative text-left whitespace-nowrap z-[94]">
-                Byoerr
-              </span>
-            </div>
-            <div className="w-full md:w-[272px] self-stretch shrink-0 font-['Poppins'] text-[20px] font-normal leading-[30px] relative text-left whitespace-nowrap z-[95]">
-              <span className="font-['Poppins'] text-[20px] font-normal leading-[30px] text-[#fff] relative text-left">
-                hello@byoerr.com
-                <br />
-              </span>
-              <span className="font-['Poppins'] text-[20px] font-normal leading-[30px] text-[#fff] relative text-left">
-                Phone : +01 23456789
-              </span>
-            </div>
-            <div className='flex w-full md:w-[272px] gap-2 items-start shrink-0 flex-nowrap opacity-70 relative z-[96]'>
-              <div className='w-[48px] h-[48px] shrink-0 relative overflow-hidden z-[97]'>
-                <div className='w-[12.994px] h-[24.021px] bg-[url(../assets/images/340805f9-4cf3-47e4-93b5-82e82f5a2ad3.png)] bg-[length:100%_100%] bg-no-repeat relative z-[98] mt-[12.051px] mr-0 mb-0 ml-[17.761px]' />
-              </div>
-              <div className='w-[48px] h-[48px] shrink-0 relative overflow-hidden z-[99]'>
-                <div className='w-[24.501px] h-[20.215px] bg-[url(../assets/images/cfb8a5b0-801b-4219-bf27-23c26377e61d.png)] bg-[length:100%_100%] bg-no-repeat relative z-[100] mt-[13.972px] mr-0 mb-0 ml-[11.521px]' />
-              </div>
-              <div className='w-[48px] h-[48px] shrink-0 bg-[url(../assets/images/228202f2-ef9a-4c64-802f-bb9a96e3f998.png)] bg-cover bg-no-repeat relative overflow-hidden z-[101]' />
-              <div className='w-[48px] h-[48px] shrink-0 relative overflow-hidden z-[102]'>
-                <div className='w-[20.087px] h-[19.217px] bg-[url(../assets/images/2a2687bd-eaac-4e9f-9a59-8c0e69dbc1d4.png)] bg-[length:100%_100%] bg-no-repeat relative z-[103] mt-[14.51px] mr-0 mb-0 ml-[13.919px]' />
-              </div>
-              <div className='w-[48px] h-[48px] shrink-0 bg-[url(../assets/images/f1a7f424-8aeb-4946-bf5e-a1772407a9be.png)] bg-cover bg-no-repeat relative overflow-hidden z-[104]' />
-            </div>
-          </div>
-          <div className='flex w-full md:w-[442px] gap-10 md:gap-40 items-start shrink-0 flex-nowrap relative z-[105] mt-10 md:mt-0'>
-            <div className='flex w-full md:w-[135px] flex-col gap-6 items-start shrink-0 flex-nowrap relative z-[106]'>
-              <span className="h-[34px] shrink-0 basis-auto font-['Inter'] text-[24px] font-semibold leading-[33.6px] text-[#fff] relative text-left whitespace-nowrap z-[107]">
-                Quick Links
-              </span>
-              <div className='flex w-full md:w-[86px] flex-col gap-4 items-start shrink-0 flex-nowrap relative z-[108]'>
-                <span className="h-[19px] shrink-0 basis-auto font-['Inter'] text-[16px] font-normal leading-[19px] text-[#fff] relative text-left whitespace-nowrap z-[109]">
-                  Services
-                </span>
-                <span className="h-[19px] shrink-0 basis-auto font-['Inter'] text-[16px] font-normal leading-[19px] text-[#fff] relative text-left whitespace-nowrap z-[110]">
-                  Portfolio
-                </span>
-                <span className="h-[19px] shrink-0 basis-auto font-['Inter'] text-[16px] font-normal leading-[19px] text-[#fff] relative text-left whitespace-nowrap z-[111]">
-                  About us
-                </span>
-                <span className="h-[19px] shrink-0 basis-auto font-['Inter'] text-[16px] font-normal leading-[19px] text-[#fff] relative text-left whitespace-nowrap z-[112]">
-                  Testimonial
-                </span>
-              </div>
-            </div>
-            <div className='flex w-full md:w-[147px] flex-col gap-6 items-start shrink-0 flex-nowrap relative z-[113]'>
-              <span className="h-[34px] shrink-0 basis-auto font-['Inter'] text-[24px] font-semibold leading-[33.6px] text-[#fff] relative text-left whitespace-nowrap z-[114]">
-                Resources
-              </span>
-              <div className='flex w-full md:w-[147px] flex-col gap-4 items-start shrink-0 flex-nowrap relative z-[115]'>
-                <span className="h-[19px] shrink-0 basis-auto font-['Inter'] text-[16px] font-normal leading-[19px] text-[#fff] relative text-left whitespace-nowrap z-[116]">
-                  Support
-                </span>
-                <span className="h-[19px] shrink-0 basis-auto font-['Inter'] text-[16px] font-normal leading-[19px] text-[#fff] relative text-left whitespace-nowrap z-[117]">
-                  Privacy Policy
-                </span>
-                <span className="h-[19px] shrink-0 basis-auto font-['Inter'] text-[16px] font-normal leading-[19px] text-[#fff] relative text-left whitespace-nowrap z-[118]">
-                  Terms & Conditions
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
+  return (
+    <footer className="bg-[#012E4D] py-[120px] px-[80px] text-white">
+      <div className="flex flex-col md:flex-row justify-between max-w-[1280px] mx-auto gap-10 md:gap-0">
+        <FooterSection>
+          <LogoSection />
+          <ContactInfo />
+          <SocialIcons />
+        </FooterSection>
+        <FooterSection>
+          <FooterLinks title="Quick Links" links={quickLinks} />
+        </FooterSection>
+        <FooterSection>
+          <FooterLinks title="Resources" links={resourcesLinks} />
+        </FooterSection>
       </div>
-    );
-  }
-  
-  export default Footer;
+    </footer>
+  );
+};
+
+const FooterSection = ({ children }) => (
+  <div className="flex flex-col items-start gap-[24px]">{children}</div>
+);
+
+const LogoSection = () => (
+  <div className="flex items-center gap-2">
+    <img src={byoerrLogo} alt="Byoerr Logo" className="cursor-pointer" />
+    <h2 className="text-[36px] font-semibold leading-[120%]">Byoerr</h2>
+  </div>
+);
+
+const ContactInfo = () => (
+  <p className="font-poppins text-[20px]">
+    hello@byoerr.com <br />
+    Phone: +01 23456789
+  </p>
+);
+
+const SocialIcons = () => (
+  <div className="flex gap-2">
+    <img src={facebookIcon} alt="Facebook" className="cursor-pointer" />
+    <img src={twitterIcon} alt="Twitter" className="cursor-pointer" />
+    <img src={instagramIcon} alt="Instagram" className="cursor-pointer" />
+    <img src={linkedinIcon} alt="LinkedIn" className="cursor-pointer" />
+    <img src={youtubeIcon} alt="YouTube" className="cursor-pointer" />
+  </div>
+);
+
+const FooterLinks = ({ title, links }) => (
+  <>
+    <h3 className="text-[24px] font-semibold leading-[140%]">{title}</h3>
+    <ul>
+      {links.map((link, index) => (
+        <li key={index} className="mb-[16px]">
+          <a href="#" className="text-[16px] font-normal no-underline text-white">
+            {link}
+          </a>
+        </li>
+      ))}
+    </ul>
+  </>
+);
+
+const quickLinks = ["Services", "Portfolio", "About us", "Testimonial"];
+const resourcesLinks = ["Support", "Privacy Policy", "Terms & Conditions"];
+
+export default Footer;
